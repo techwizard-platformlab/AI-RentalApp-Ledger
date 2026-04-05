b999d9540cb8eb3091b46a4a756012e3f8a24ea7
Ramprasath26 <ramprasath2691@outlook.com>
2026-04-05 16:12:14 +0530

    fix: remove invalid semicolons from terraform variable blocks

--- terraform/azure/modules/load_balancer/variables.tf
+++ terraform/azure/modules/load_balancer/variables.tf
@@ -1,5 +1,20 @@
-variable "environment"         { type = string }
-variable "location"            { type = string }
-variable "location_short"      { type = string }
-variable "resource_group_name" { type = string }
-variable "tags"                { type = map(string); default = {} }
+variable "environment" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "location_short" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
