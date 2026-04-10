5f7eb76cbb10b6dd2daf06fd02d25d881c057adf
Ramprasath26 <ramprasath2691@outlook.com>
2026-04-10 23:06:38 +0530

    Adding OPA for infra cost

--- infrastructure/azure/modules/budget/outputs.tf
+++ infrastructure/azure/modules/budget/outputs.tf
@@ -1,9 +1,9 @@
 output "budget_id" {
   description = "Resource ID of the consumption budget"
-  value       = azurerm_consumption_budget_resource_group.weekly.id
+  value       = azurerm_consumption_budget_resource_group.monthly.id
 }
 
 output "budget_name" {
   description = "Name of the consumption budget"
-  value       = azurerm_consumption_budget_resource_group.weekly.name
+  value       = azurerm_consumption_budget_resource_group.monthly.name
 }
