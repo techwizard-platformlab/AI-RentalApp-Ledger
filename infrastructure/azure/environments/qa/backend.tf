1db1beb67158623e014a25c0df4628fc449cf15b
Ramprasath26 <ramprasath2691@outlook.com>
2026-04-18 10:46:22 +0530

    login issue fixed

--- infrastructure/azure/environments/qa/backend.tf
+++ infrastructure/azure/environments/qa/backend.tf
@@ -4,7 +4,8 @@ terraform {
     #   resource_group_name  = TF_BACKEND_RG  (techwizard-platformlab-apps)
     #   storage_account_name = TF_BACKEND_SA  (techwizardappstfstate)
     #   container_name       = rentalapp-qa-tfstate
-    key      = "terraform.tfstate"
-    use_oidc = true
+    key               = "terraform.tfstate"
+    use_oidc          = true
+    use_azuread_auth  = true
   }
 }
